17b46fa68974d7f7da6f044c13e8c95785386311
00andobe <00andobe@skola.boras.se>
2023-01-23 09:58:33 +0100

    Printar ut land region och population

--- app.ts
+++ app.ts
@@ -1,6 +1,31 @@
+interface Country {
+    name: string;
+    region: string;
+    population: number;
+}
+
+
 async function foo() {
     const response = await fetch("https://restcountries.com/v3.1/all");
     const data = await response.json();
-    console.log(data);
+    let dataPrint : any = document.getElementById("dataPrint") as HTMLElement;
+    const countries = data.filter((item: Country) => item.region !== ' ');
+    const fragment = document.createDocumentFragment();
+    console.log(data)
+    
+    countries.forEach((country : any )=> {
+        const div = document.createElement('div');
+        div.classList.add('country');
+        div.innerHTML = `<h2>${country.name.common}</h2>
+                        <p>Region: ${country.region}</p>
+                        <p>Population: ${country.population}</p>`;
+        fragment.appendChild(div);
+    });
+    dataPrint.appendChild(fragment);
+    
+
+
+    // dataPrint.innerText = JSON.stringify(data);
+    // console.log(JSON.stringify(data));
 }
 foo();
